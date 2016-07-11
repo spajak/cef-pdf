1bc4bf773380e38993efdcd1339fba0c268b42c8
Sebastian Pająk <spconv@gmail.com>
2016-07-11 09:15:25 +0200

    Added separate PdfPrintCallback

---
+++ src/PdfPrintCallback.cpp
@@ -0,0 +1,19 @@
+#include "PdfPrintCallback.h"
+
+PdfPrintCallback::PdfPrintCallback(CefRefPtr<CefBrowser> browser)
+{
+    m_browser = browser;
+}
+
+void PdfPrintCallback::OnPdfPrintFinished(const CefString& path, bool ok)
+{
+    DLOG(INFO)
+        << "OnPdfPrintFinished"
+        << ", path: " << path.ToString()
+        << ", ok: " << ok;
+
+    if (m_browser.get()) {
+        m_browser->GetHost()->CloseBrowser(false);
+        m_browser = NULL;
+    }
+}
