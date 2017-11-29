e21b83768f17dac112b392b70db6831f807ae8fa
Becky Conning <becky.conning@icloud.com>
2017-11-29 21:44:58 +0000

    Added line endings to remote trigger help

--- src/main.cpp
+++ src/main.cpp
@@ -43,11 +43,11 @@ void printHelp(std::string name)
     std::cout << "                   If omitted some default margin is applied." << std::endl;
     std::cout << "  --javascript     Enable JavaScript." << std::endl;
     std::cout << "  --backgrounds    Print with backgrounds. Default is without." << std::endl;
-    std::cout << "  --remote-trigger Defer printing until page evaluates:";
-    std::cout << "                   window.cefPdfQuery({request: \"trigger\", onSuccess: function () {}, onFailure: function () {}});";
-    std::cout << "                   Will abort immediately if the page doesn't evaluate:";
-    std::cout << "                   window.cefPdfQuery({request: \"register\", onSuccess: function () {}, onFailure: function () {}});";
-    std::cout << "                   synchronously during load (e.g. in a script tag in the body).";
+    std::cout << "  --remote-trigger Defer printing until page evaluates:" << std::endl;
+    std::cout << "                   window.cefPdfQuery({request: \"trigger\", onSuccess: function () {}, onFailure: function () {}});" << std::endl;
+    std::cout << "                   Will abort immediately if the page doesn't evaluate:" << std::endl;
+    std::cout << "                   window.cefPdfQuery({request: \"register\", onSuccess: function () {}, onFailure: function () {}});" << std::endl;
+    std::cout << "                   synchronously during load (e.g. in a script tag in the body)." << std::endl;
     std::cout << std::endl;
     std::cout << "Server options:" << std::endl;
     std::cout << "  --server         Start HTTP server" << std::endl;
